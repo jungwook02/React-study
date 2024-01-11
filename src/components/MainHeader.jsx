79f4b0136363570840b4bd1119772f07770cccbd
jungwook02 <aking0520@naver.com>
2024-01-11 17:52:53 +0900

    0111

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,21 +1,22 @@
-import {MdPostAdd,MdMessage} from 'react-icons/md';
-
+import { MdPostAdd, MdMessage } from 'react-icons/md';
+import {Link} from 'react-router-dom';
 import classes from './MainHeader.module.css';
 
-function MainHeader({onCreatePost}) {
-    return (
-        <header className={classes.header}>
-            <h1 className={classes.logo}>
-                <MdMessage />
-                React Poster
-            </h1>
-            <p>
-                <button className={classes.button} onClick={onCreatePost}>
-                    <MdPostAdd size={18} />
-                </button>
-            </p>
-        </header>
-    )
+function MainHeader({ onCreatePost }) {
+  return (
+    <header className={classes.header}>
+      <h1 className={classes.logo}>
+        <MdMessage />
+        React Poster
+      </h1>
+      <p>
+        <Link to="/create-post" className={classes.button} onClick={onCreatePost}>
+          <MdPostAdd size={18} />
+          New Post
+        </Link>
+      </p>
+    </header>
+  );
 }
 
 export default MainHeader;
